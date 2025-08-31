7340fac883720a6b4dfa72c3203a774f8e6e3dc6
Branko Pavlović <copa1313@gmail.com>
2025-08-31 14:40:17 +0000

    povećaj logotip za 96%

--- src/components/zodiac-wheel.tsx
+++ src/components/zodiac-wheel.tsx
@@ -101,7 +101,7 @@ export function ZodiacWheel({ signs, onSelect, selectedValue, disabled, label, l
                             />
                          </div>
                     ) : (
-                        <Logo className="w-32 h-32 text-primary/40 animate-in fade-in" />
+                        <Logo className="w-52 h-52 text-primary/40 animate-in fade-in" />
                     )}
                 </div>
 
