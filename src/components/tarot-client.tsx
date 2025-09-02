5c4297ee20d6f78d19945c4ea1ebd5faa10dd98d
Branko Pavlović <copa1313@gmail.com>
2025-09-02 12:16:34 +0000

    i dalje je logotip jako mali i manji nego što je bio

--- src/components/tarot-client.tsx
+++ src/components/tarot-client.tsx
@@ -286,8 +286,8 @@ React.useEffect(() => {
            <div className="relative">
              {isReadyForNewReading ? (
                <>
-                 <Button variant="ghost" size="icon" onClick={resetForm} className="block sm:hidden text-primary hover:bg-primary/10 h-16 w-16 p-0">
-                   <Logo className="h-full w-full p-1" />
+                 <Button variant="ghost" onClick={resetForm} className="block sm:hidden text-primary hover:bg-primary/10 h-12 w-12">
+                   <Logo className="h-10 w-10" />
                    <span className="sr-only">Novo čitanje</span>
                  </Button>
                  <Button onClick={resetForm} className="hidden sm:flex" variant="default" size="sm">
@@ -575,3 +575,5 @@ React.useEffect(() => {
     </div>
   );
 }
+
+    
