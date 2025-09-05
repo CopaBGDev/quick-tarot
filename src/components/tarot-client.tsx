594e0957cc29d9ad5082edc14d91ba5b992c3be2
Branko Pavlović <copa1313@gmail.com>
2025-09-05 17:03:03 +0000

    ne interesuje me kako ćeš ovo rešiti ali želim da podesiš ispravan raspo

--- src/components/tarot-client.tsx
+++ src/components/tarot-client.tsx
@@ -533,7 +533,7 @@ export default function TarotClient() {
                 onSubmit={form.handleSubmit(onSubmit)}
                 className="w-full max-w-5xl mx-auto flex flex-col xl:grid xl:grid-cols-[472px_1fr] xl:items-start xl:gap-8"
               >
-                <div className="w-full xl:order-2">
+                <div className="w-full xl:order-2 flex flex-col">
                     <header className="flex w-full flex-col items-center text-center xl:text-left xl:items-start">
                         <div className="flex flex-col items-center xl:items-start">
                             <Logo className="h-28 w-28 text-primary" />
@@ -545,7 +545,18 @@ export default function TarotClient() {
                             {translations.headerSubtitle}
                         </p>
                     </header>
-                     <div className="w-full max-w-md space-y-8 mt-12 xl:mt-8 mx-auto xl:mx-0 xl:max-w-none">
+                    <div className="w-full mt-12 xl:hidden">
+                        <div className="flex flex-col items-center">
+                            <ZodiacWheel
+                                signs={zodiacSigns}
+                                onSelect={setSelectedZodiacSign}
+                                selectedValue={selectedZodiacSign}
+                                disabled={disabled}
+                            />
+                            {zodiacError && <p className="text-center mt-4 text-sm font-medium text-destructive">{zodiacError}</p>}
+                        </div>
+                    </div>
+                     <div className="w-full max-w-md space-y-8 mt-12 mx-auto xl:mx-0 xl:max-w-none">
                         <FormField
                           control={form.control}
                           name="question"
@@ -591,7 +602,7 @@ export default function TarotClient() {
                     </div>
                 </div>
 
-                <div className="w-full mt-12 xl:mt-0 xl:order-1">
+                <div className="w-full mt-12 xl:mt-0 xl:order-1 hidden xl:flex">
                   <div className="flex flex-col items-center">
                       <ZodiacWheel
                         signs={zodiacSigns}
@@ -614,3 +625,5 @@ export default function TarotClient() {
     </div>
   );
 }
+
+    
