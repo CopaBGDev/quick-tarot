9a65c3bef7b1750b9c86e1a81863be5d1ad65b80
Branko Pavlović <copa1313@gmail.com>
2025-09-07 00:16:37 +0000

    [19:45:09.684] Running build in Washington, D.C., USA (East) – iad1 [19:

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '4.5mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ovi moduli su opcione zavisnosti Genkita i nisu potrebni u produkciji,
+      // ali Next.js pokušava da ih uključi. Ovim ih eksplicitno isključujemo.
+      config.externals.push('@opentelemetry/exporter-jaeger');
+      config.externals.push('@genkit-ai/firebase');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
