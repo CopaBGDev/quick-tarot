a5cbce869fcec005f23b319a36068906e9cb6f26
Branko Pavlović <copa1313@gmail.com>
2025-09-08 14:48:47 +0000

    želim

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2968048666598097"
           crossOrigin="anonymous"
-          strategy="lazyOnload"
+          strategy="beforeInteractive"
         />
         <GoogleAnalytics />
       </head>
