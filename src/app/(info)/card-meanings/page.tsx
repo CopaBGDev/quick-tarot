9a29739f886927dbee78d613f98c6fd49d6c57e9
Branko Pavlović <copa1313@gmail.com>
2025-09-17 20:41:26 +0000

    jel tebi ovo izgleda ok?

--- src/app/(info)/card-meanings/page.tsx
+++ src/app/(info)/card-meanings/page.tsx
@@ -18,7 +18,7 @@ export default function CardMeaningsPage({ searchParams }: { searchParams: { lan
                   src={getCardImagePath(cardName)}
                   alt={cardName}
                   fill
-                  className="object-contain object-top"
+                  className="object-cover"
                   sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, 25vw"
               />
             </div>
